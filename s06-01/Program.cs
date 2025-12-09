1d6230224dfff899de45afeb2f343794c2c53c55
Hosna <h.m.hosna@gmail.com>
2025-12-09 22:42:04 +0330

    .

---
+++ s06-01/Program.cs
@@ -0,0 +1,7 @@
+﻿var path = @"C:\Users\Lenovo\Desktop\input6.txt";
+
+string[] lines = File.ReadAllLines(path);
+
+int width  = lines.Max(l => l.Length);
+lines = lines.Select(l => l.PadRight(width)).ToArray();
+
